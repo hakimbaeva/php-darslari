824cde07ffed08c647b62ca8900a8a4d90cac5fe
hakimbaeva <hakimboevasonajon@gmail.com>
2025-11-15 15:37:58 +0500

    Date and time

--- data.vs.time.php
+++ data.vs.time.php
@@ -16,7 +16,14 @@ echo date("d.F.y"). "<br>" ;
 echo time(). "<br>";
 echo date("d.m.Y"). "<br>";
 echo date("d.m.Y " ,  strtotime("2025-11-23")  ). "<br>";
-
-
-
+// 15.11.2025
+echo strtotime("+7") ;
+echo strtotime("d.m.Y H:i" ,strtotime("+7 days")). "<br>" ;
+echo strtotime("Y-m-d " ,strtotime("last monday")). "<br>" ;
+echo strtotime("d.m.Y " ,strtotime("next year")). "<br>" ;
+echo strtotime("d.m.Y " ,strtotime("2024-11-14")). "<br>" ;
+$t_yil ="2011-12-13";
+$yosh =(time() - strtotime($t_yil)) / (60 * 60 * 24 * 365) ;
+echo (int)$yosh ;
+echo strtotime("d.m.Y " ,strtotime("-10 days")). "<br>" ;
 ?>
